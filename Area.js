e7fb211fcc5a450f06b773fa6f15b9b1734cce02
Sumit Majukar <164126462+Majukar72plus@users.noreply.github.com>
2025-04-07 20:00:20 +0530

    Add files via upload

---
+++ Area.js
@@ -0,0 +1,8 @@
+
+function calculateRectangleArea(length, width) {
+    return length * width;
+  }
+  const length = 10;
+  const width = 5;
+  const area = calculateRectangleArea(length, width);
+  
