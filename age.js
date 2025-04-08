af56f4e2cc378a06a5612892cf4f2b54a0345f55
Sumit Majukar <164126462+Majukar72plus@users.noreply.github.com>
2025-04-08 20:21:29 +0530

    Add files via upload

---
+++ age.js
@@ -0,0 +1,11 @@
+function calculateAge(birthYear, currentYear) {
+    var age = currentYear - birthYear;
+    return "You are " + age + " years old.";
+  }
+  console.log(calculateAge(1990, 2024));
+  console.log(calculateAge(2000, 1995));
+  console.log(calculateAge(1995, 2024));
+  console.log(calculateAge(1995, 2024)); 
+
+
+  
